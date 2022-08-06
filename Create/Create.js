c0d2af2c6d87e42ab68e2bf8a848f82e4463afed
mimoza89 <79771345+mimoza89@users.noreply.github.com>
2022-08-06 23:28:17 +0300

    create functionality update

--- Create/Create.js
+++ Create/Create.js
@@ -1,11 +1,28 @@
-
+import { useContext } from 'react';
+import { DataContext } from '../../contexts/DataContext';
+import * as playersService from '../../services/dataService';
 import styles from './Create.module.css';
 
 const CreatePost = () => {
+    const { playerAdd } = useContext(DataContext)
+
+    const onSubmit = (e) => {
+        e.preventDefault();
+        const playerInfo =  Object.fromEntries(new FormData(e.target))
+
+        playersService.create(playerInfo)
+            .then(result => {
+                playerAdd(result)
+            })
+    }
     return (
         <section className={styles['create-post']}>
-            <form className={styles['create-form']} onSubmit={onSubmit}>
+            <form className={styles['create-form']} onSubmit={onSubmit} >
                 <div className={styles['create-container']}>
+                <div>
+                        <label htmlFor="imageUrl"> Photo: </label>
+                        <input className={styles.input} type="text" id="imageUrl" name="imageUrl" defaultValue={''} />
+                    </div>
                     <div>
                         <label htmlFor="name">Name: </label>
                         <input className={styles.input} type="text" id="name" name="name" defaultValue={''} />
@@ -21,6 +38,10 @@ const CreatePost = () => {
                     <div>
                         <label htmlFor="mostSuccessfulTournament"> Most successful Tournament: </label>
                         <input className={styles.input} type="text" id="mostSuccessfulTournament" name="mostSuccessfulTournament" defaultValue={''}/>
+                    </div>
+                    <div>
+                        <label htmlFor="description"> More info: </label>
+                        <input className={styles.input} type="text" id="description" name="description" defaultValue={''}/>
                     </div>
 
                     <button className={styles.button}> Submit</button>
